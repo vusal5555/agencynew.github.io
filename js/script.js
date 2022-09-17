9b4a7dc030fc220dd71b760d5889059d2f077036
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2022-09-18 00:40:39 +0400

    made a new navbar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-let mainMenu = document.querySelector(".header--nav-nav");
-const closeMenu = document.querySelector(".closemenu");
-const openMenu = document.querySelector(".openmenu");
+// const mainMenu = document.querySelector(".header--nav-nav");
+// const closeMenu = document.querySelector(".closemenu");
+// const openMenu = document.querySelector(".openmenu");
 
-openMenu.addEventListener("click", showm);
-closeMenu.addEventListener("click", closeM);
+// openMenu.addEventListener("click", showm);
+// closeMenu.addEventListener("click", closeM);
 
-function showm() {
-  mainMenu.style.display = "flex";
-  mainMenu.style.top = "0";
+// function showm() {
+//   mainMenu.style.display = "flex";
+//   mainMenu.style.top = 0;
+// }
+
+// function closeM() {
+//   mainMenu.style.top = "-110%";
+// }
+
+// const hamburger = document.getElementById("hamburger");
+// const navUl = document.getElementById("navUl");
+
+// hamburger.addEventListener("click", () => {
+//   navUl.classList.toggle("show");
+//   navUl.style.transition = "0.5s";
+// });
+
+// const hamburger = document.querySelector(".openmenu");
+// const navMenu = document.querySelector(".header--nav-nav");
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
+
+const navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.left = "0";
 }
-function closeM() {
-  mainMenu.style.top = "-110%";
+function hideMenu() {
+  navLinks.style.left = "-300px";
 }
