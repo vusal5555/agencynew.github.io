ae090f02ecae4ce901decdae1a43157ac025814d
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2022-12-10 19:50:39 +0400

    opt80923947801341

--- js/script.js
+++ js/script.js
@@ -94,7 +94,6 @@ const navSticky = function (entries) {
 const navObserver = new IntersectionObserver(navSticky, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px`,
 });
 
 navObserver.observe(header);
